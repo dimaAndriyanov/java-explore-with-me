8d11b1a8909de1f9ab89d9dc15f86717db4ef9e4
Dmitriy <d.79052201159@yandex.ru>
2023-10-26 00:22:51 +0300

    style: fixed some typos refactor: StatClient now is context component

--- stat-service/stat-client/src/main/java/ru/practicum/ewm/StatClient.java
+++ stat-service/stat-client/src/main/java/ru/practicum/ewm/StatClient.java
@@ -2,9 +2,12 @@ package ru.practicum.ewm;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
@@ -14,17 +17,24 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+@Service
 public class StatClient {
     private final RestTemplate rest;
+    private final ObjectMapper objectMapper;
     private final String appName;
 
     private static final String HEADER_USER_APP = "X-Stat-Server-User-App";
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public StatClient(String serverUrl, String appName) {
-        rest = new RestTemplateBuilder()
+    @Autowired
+    public StatClient(@Value("${stat-server.url}") String serverUrl,
+                      @Value("${stat-server.user-app-name}") String appName,
+                      RestTemplateBuilder builder, ObjectMapper objectMapper) {
+        rest = builder
                 .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
                 .requestFactory(HttpComponentsClientHttpRequestFactory::new)
                 .build();
+        this.objectMapper = objectMapper;
         this.appName = appName;
     }
 
@@ -40,7 +50,7 @@ public class StatClient {
         }
 
         if (!statServerResponse.getStatusCode().is2xxSuccessful()) {
-            String message = statServerResponse.getBody() == null ? "Internal error has occured on stat server" :
+            String message = statServerResponse.getBody() == null ? "Internal error has occurred on stat server" :
                     statServerResponse.getBody().toString();
             throw new CouldNotSaveHitException(message);
         }
@@ -54,8 +64,8 @@ public class StatClient {
         UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder
                 .newInstance()
                 .path("/stats")
-                .queryParam("start", start.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
-                .queryParam("end", end.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+                .queryParam("start", start.format(DATE_TIME_FORMATTER))
+                .queryParam("end", end.format(DATE_TIME_FORMATTER));
         if (unique != null) {
             uriComponentsBuilder.queryParam("unique", unique);
         }
@@ -72,12 +82,12 @@ public class StatClient {
             throw new CouldNotGetStatsException(exception.getResponseBodyAsString());
         }
         if (!statServerResponse.getStatusCode().is2xxSuccessful()) {
-            String message = statServerResponse.getBody() == null ? "Internal error has occured on stat server" :
+            String message = statServerResponse.getBody() == null ? "Internal error has occurred on stat server" :
                     statServerResponse.getBody();
             throw new CouldNotGetStatsException(message);
         }
         try {
-            return new ObjectMapper().readValue(statServerResponse.getBody(), new TypeReference<>() {});
+            return objectMapper.readValue(statServerResponse.getBody(), new TypeReference<>() {});
         } catch (Throwable exception) {
             throw new CouldNotGetStatsException("Could not read response from stat server");
         }
